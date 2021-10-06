912a03d493f38ede2b88c8fbbab8d7640a144f44
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2021-10-07 01:59:03 +0200

    Setup project

---
+++ vue.config.js
@@ -0,0 +1,28 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+    configureWebpack: {
+        plugins: [
+            new HtmlWebpackPlugin({
+                title: "Frontend Mentor | 3-column preview card component",
+                template: "public/index.html"
+            })
+        ],
+        resolve: {
+            alias: {
+                "@": path.resolve("src")
+            }
+        }
+    },
+    css: {
+        loaderOptions: {
+            sass: {
+                prependData: `
+                    @use "~@/assets/scss/utilities/colors";
+                    @use "~@/assets/scss/utilities/fonts";
+                `
+            }
+        }
+    }
+}
